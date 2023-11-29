d7024701d94ffdb73fa61d6b1ba886dfc670cabe
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-11-29 16:43:55 +0100

    pipa: Link dolby blobs against v33 libstagefright_foundation
     * fixes crashes and absurdly high CPU usage while using dolby
    
    Co-authored-by: Woomymy <woomy@woomy.be>
    Change-Id: Ib72b775dae677f3085d0b0e2afcbb709d8690063
    Signed-off-by: basamaryan <basam.aryan@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -61,6 +61,9 @@ function blob_fixup() {
         vendor/etc/init/init.batterysecret.rc)
             sed -i "/seclabel u:r:batterysecret:s0/d" "${2}"
             ;;
+        vendor/lib/libstagefright_soft_ac4dec.so | vendor/lib/libstagefright_soft_ddpdec.so | vendor/lib64/libdlbdsservice.so)
+            "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
+            ;;
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
             "${SIGSCAN}" -p "9A 0A 00 94" -P "1F 20 03 D5" -f "${2}"
             ;;
