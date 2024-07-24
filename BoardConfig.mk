fa60b5aad057b55f16f78c3da8dd9a171aefa615
AdarshGrewal <adarshgrewal@gmail.com>
2024-07-25 01:20:49 +0530

    pipa: get rid of all thing NFC
    Change-Id: I2d91d940897a2823b735951f3a90162627d512fb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -187,8 +187,6 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/compatibility_matrix.xml
-ODM_MANIFEST_SKUS += nfc
-ODM_MANIFEST_NFC_FILES := $(DEVICE_PATH)/manifest_nfc.xml
 
 # Wi-Fi
 BOARD_WLAN_DEVICE := qcwcn
