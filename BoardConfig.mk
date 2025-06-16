674d5d83beb5449ab362ada432f193b9fc9407fe
AdarshGrewal <adarshgrewal@gmail.com>
2025-06-16 22:40:59 +0530

    pipa: Upgrade from OS2.0.4.0.UMZMIXM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -138,7 +138,7 @@ TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2025-03-01
+VENDOR_SECURITY_PATCH := 2025-05-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
