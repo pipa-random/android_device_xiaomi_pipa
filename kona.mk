247bc2bfe75500a6c92e2f428cf67761ff565b73
AdarshGrewal <adarshgrewal@gmail.com>
2026-05-19 20:02:43 +0530

    pipa: Convert overlays to rros
    Change-Id: I098d519fbae79e1d9df47e137fcb0401e78406d1

--- kona.mk
+++ kona.mk
@@ -217,11 +217,15 @@ PRODUCT_PACKAGES += \
 TARGET_SUPPORTS_OMX_SERVICE := false
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
-
-PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_PACKAGES += \
+    ApertureOverlayPipa \
+    FrameworkOverlayPipa \
+    LineageSDKOverlayPipa \
+    LineageSettingsOverlayPipa \
+    LineageSystemUIOverlayPipa \
+    SettingsOverlayPipa \
+    SettingsProviderOverlayPipa \
+    SystemUIOverlayPipa
 
 # Partitions
 PRODUCT_PACKAGES += \
